4bb63bbac2b7a9518b0f342eba91f7a1511c1f93
Yushi Yang <yangyushi1992@icloud.com>
2021-04-18 12:59:04 +0100

    new hist match to fix intensity drift

--- nplocate/__init__.py
+++ nplocate/__init__.py
@@ -1,4 +1,4 @@
 from .utility import refine, add, see_slice, see_particle,\
-    get_gr, simulate, fix_intensity, save_xyz, get_gr_pyhs
+    get_gr, simulate, fix_intensity, save_xyz, get_gr_pyhs, hist_match
 from .characterise import ParticleFeatures, feature_plot_2d
 from .model import GaussianSphere
